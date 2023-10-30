4b8908bba11bcce703ad50fb0b3c6ff7a0a7aea6
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-10-30 14:27:21 +0530

    CRUD operation

--- src/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export const getData = () => {
-    return axios.get(" http://localhost:3030/posts").then((res)=>{
-        // console.log(res.data);
-        return res.data;
-    });
-}
